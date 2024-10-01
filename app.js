34c3f1ecef9f172c39ca4c04c037f77a08551618
jnl-2002 <jnl9327@naver.com>
2024-10-01 16:41:37 +0900

    Book-shop

---
+++ app.js
@@ -0,0 +1,19 @@
+// express 모듈
+const express = require ('express');
+const app = express();
+
+// dotenv 모듈
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.listen(process.env.PORT);
+
+const userRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+const likesRouter = require('./routes/likes');
+const ordersRouter = require('./routes/orders');
+
+app.use('/users', userRouter);
+app.use('/books', booksRouter);
+app.use('/likes', likesRouter);
+app.use('/orders', ordersRouter);
