22c05863e234eeca54bf8af72f7abdd7146ca892
jnl-2002 <jnl9327@naver.com>
2024-10-16 19:22:02 +0900

    JWT 회원 정보 가져오기 추가

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,49 +1,79 @@
 const conn = require('../db');
 const {StatusCodes} = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.addToCart =  (req, res) => {
-    const {book_id,quantity,user_id} = req.body;
+    const {book_id,quantity} = req.body;
 
-    let sql = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?)`;
-    let values = [book_id, quantity, user_id];
-    conn.query(sql, values,
-        (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+    let authorization = ensureAuthorization(req, res);
 
-        return res.status(StatusCodes.OK).json(result);
-    })
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 다시 하세용~"
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰이에용~"
+        });
+    } else  {
+        let sql = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES (?, ?, ?)`;
+        let values = [book_id, quantity, authorization.id];
+        conn.query(sql, values,
+            (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+    
+            return res.status(StatusCodes.OK).json(result);
+        })}
 };
 
 exports.getCartItems =  (req, res) => {
-    const {user_id, selected} = req.body;
+    const {selected} = req.body;
 
-    let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price
-            FROM cartItems LEFT JOIN books
-            ON cartItems.book_id = books.id
-            WHERE user_id=? AND cartItems.id IN (?)`;
-    let values = [user_id, selected];
+    let authorization = ensureAuthorization(req, res);
+
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 다시 하세용~"
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰이에용~"
+        });
+    } else {
+        let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price
+        FROM cartItems LEFT JOIN books
+        ON cartItems.book_id = books.id
+        WHERE user_id=? AND cartItems.id IN (?)`;
+
+    let values = [authorization.id, selected];
     conn.query(sql, values,
-           (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+       (err, result) => {
+    if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+    }
 
-        if(result[0])
-            return res.status(StatusCodes.OK).json(result[0]);
-        else
-            return res.status(StatusCodes.NOT_FOUND).end();
-    });
+    if(result[0])
+        return res.status(StatusCodes.OK).json(result[0]);
+    else
+        return res.status(StatusCodes.NOT_FOUND).end();
+
+    })
+    };
+
+    
 };
 
 exports.removeCartItem =  (req, res) => {
-    const {id} = req.params;
+    const cartItemId = req.params.id;
 
     let sql = `DELETE FROM cartItems WHERE id = ?`;
-    conn.query(sql, id,
+    conn.query(sql, cartItemId,
         (err, result) => {
         if (err) {
             console.log(err);
@@ -54,4 +84,14 @@ exports.removeCartItem =  (req, res) => {
     })
 };
 
+function ensureAuthorization (req, res) {
+    try {
+        let receuvedJwt = req.headers["authorization"];
+        let decodedJwt = jwt.verify(receuvedJwt, process.env.PRIVATE_KEY);
+        return decodedJwt;
+    } catch (err) {
+        return err
+    }
+    
+};
 
--- controller/likeController.js
+++ controller/likeController.js
@@ -1,13 +1,26 @@
+const jwt = require('jsonwebtoken');
 const conn = require('../db');
 const {StatusCodes} = require('http-status-codes');
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.addLike =  (req, res) => {
     
-    const {id} = req.params;
-    const {user_id} = req.body;
+    const book_id = req.params;
 
+    let authorization = ensureAuthorization(req, res);
+
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 다시 하세용~"
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰이에용~"
+        });
+    } else  {
     let sql = `INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)`;
-    let values = [user_id, id];
+    let values = [authorization.id, book_id];
     conn.query(sql, values,
         (err, result) => {
         if (err) {
@@ -16,16 +29,29 @@ exports.addLike =  (req, res) => {
         }
 
         return res.status(StatusCodes.OK).json(result);
-    })
+    
+        })
+    }
 };
+
 
 exports.removeLike = (req, res) => {
 
-    const {id} = req.params;
-    const {user_id} = req.body;
+    const book_id = req.params.id;
 
+    let authorization = ensureAuthorization(req, res);
+
+    if (authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 다시 하세용~"
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            "message" : "잘못된 토큰이에용~"
+        });
+    } else  {
     let sql = `DELETE FROM likes WHERE user_id = ? AND liked_book_id = 3;`;
-    let values = [user_id, id];
+    let values = [authorization.id, book_id];
     conn.query(sql, values,
         (err, result) => {
         if (err) {
@@ -34,6 +60,16 @@ exports.removeLike = (req, res) => {
         }
 
             return res.status(StatusCodes.OK).json(result);
-    })
+        })
+    }
 };
 
+function ensureAuthorization (req, res) {
+    try {
+        let receuvedJwt = req.headers["authorization"];
+        let decodedJwt = jwt.verify(receuvedJwt, process.env.PRIVATE_KEY);
+        return decodedJwt;
+    } catch (err) {
+        return err
+    }
+};
